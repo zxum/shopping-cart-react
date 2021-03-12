9c3139247a801379cbc1e3dedfd3522dfde2b0bc
zxum <zion.um@outlook.com>
2021-03-12 12:09:17 -0600

    Add basic navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Navbar() {
   return (
     <nav>
-      
+      <div className="flex-nav">
+        <div className="flex-group">
+          <Link to='/'>HOME</Link>
+          <Link to='/shop'>SHOP</Link>
+        </div>
+        <h1 className="logo">WANDERFUL</h1>
+          <Link to='/cart'>CART</Link>
+      </div>
     </nav>
   )
 }
